be54a96642953405687f746af81c3f1800f34e91
Alexander Glushko <aglushko2@slb.ru>
2025-08-25 13:03:23 +0300

    feat: add debug config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local dap = require("dap")
+
+table.insert(dap.configurations.python, {
+  type = "python",
+  request = "attach",
+  name = "attach to litstar in docker",
+  connect = {
+    host = "127.0.0.1",
+    port = 5678,
+  },
+  mode = "remote",
+  cwd = vim.fn.getcwd(),
+  pathMappings = {
+    { localRoot = vim.fn.getcwd(), remoteRoot = "/server" },
+  },
+  justMyCode = false,
+})
